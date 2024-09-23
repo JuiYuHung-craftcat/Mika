b930a2590ab4253fd0c78cec07809a3444614574
JuiYuHung-craftcat <juiyuhunghub@gmail.com>
2024-09-23 20:11:02 +0800

    Add submodule spdlog and add Log api

--- Mika/src/Mika/EntryPoint.h
+++ Mika/src/Mika/EntryPoint.h
@@ -6,6 +6,11 @@ extern Mika::Application* Mika::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Mika::Log::Init();
+	MIKA_CORE_WARN("Initialized Log!");
+	int a = 5;
+	MIKA_INFO("Hello! Var={0}", a);
+
 	auto app = Mika::CreateApplication();
 	app->Run();
 	delete app;
---
+++ Mika/src/Mika/Log.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "Core.h"
+#include "spdlog\spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace Mika {
+
+	class MIKA_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+
+}
+
+// Core log macros
+#define MIKA_CORE_TRACE(...)   ::Mika::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define MIKA_CORE_INFO(...)    ::Mika::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define MIKA_CORE_WARN(...)    ::Mika::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define MIKA_CORE_ERROR(...)   ::Mika::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define MIKA_CORE_FATAL(...)   ::Mika::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+// Client log macros
+#define MIKA_TRACE(...)        ::Mika::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define MIKA_INFO(...)         ::Mika::Log::GetClientLogger()->info(__VA_ARGS__)
+#define MIKA_WARN(...)         ::Mika::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define MIKA_ERROR(...)        ::Mika::Log::GetClientLogger()->error(__VA_ARGS__)
+#define MIKA_FATAL(...)        ::Mika::Log::GetClientLogger()->fatal(__VA_ARGS__)
