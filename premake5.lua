15828ca76173ec49fb9232955cf6701374d9747c
JuiYuHung-craftcat <juiyuhunghub@gmail.com>
2024-09-29 08:17:46 +0800

    Add GLFW static lib

--- premake5.lua
+++ premake5.lua
@@ -10,6 +10,12 @@ workspace "Mika"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
+IncludeDir["GLFW"] = "Mika/vendor/GLFW/include"
+
+include "Mika/vendor/GLFW"
+
 project "Mika"
 	location "Mika"
 	kind "SharedLib"
@@ -30,7 +36,14 @@ project "Mika"
 	includedirs
 	{
 		"%{prj.name}/src",
-		"%{prj.name}/vendor/spdlog/include"
+		"%{prj.name}/vendor/spdlog/include",
+		"%{IncludeDir.GLFW}"
+	}
+
+	links
+	{
+		"GLFW",
+		"opengl32.lib"
 	}
 
 	filter "system:windows"
